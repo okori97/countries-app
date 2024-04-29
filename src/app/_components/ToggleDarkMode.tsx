60c4c3090872844dd5ac62eb92e07dbf68d6ac6a
Okori Lewis-McCalla <okorishakir@gmail.com>
2024-04-29 21:52:09 +0100

    feat: get toggle switching darkmode on/off

--- src/app/_components/ToggleDarkMode.tsx
+++ src/app/_components/ToggleDarkMode.tsx
@@ -1,19 +1,31 @@
+"use client";
+
 import Image from "next/image";
+import { useAppContext } from "../context/state";
 import darkModeIcon from "../../../public/images/icon-dark-mode.svg";
 
 export function ToggleDarkMode() {
+  const { darkMode, setDarkMode } = useAppContext();
+
+  const handleClick = () => {
+    setDarkMode(!darkMode);
+  };
+
   return (
     <button className="cursor-pointer">
-      <div className="flex flex-row items-center gap-2">
+      <div
+        className="flex flex-row items-center gap-2 dark:text-white"
+        onClick={handleClick}
+      >
         <Image
-          className=" h-4"
+          className=" "
           // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
           src={darkModeIcon}
           alt="dark mode"
           height="20"
           width="15"
         />
-        <p>Dark Mode</p>
+        {darkMode == false ? <p>Dark Mode</p> : <p>Light Mode</p>}
       </div>
     </button>
   );
